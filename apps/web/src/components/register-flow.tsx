80cc49d58d378264493c39e3444946c33d904f56
ericzakariasson <eric.zakariasson@gmail.com>
2023-11-18 17:32:13 +0300

    chore: update unlimit

--- apps/web/src/components/register-flow.tsx
+++ apps/web/src/components/register-flow.tsx
@@ -8,11 +8,12 @@ export function RegisterFlow() {
   const sdk = useRef<GateFiSDK>();
   useEffect(() => {
     sdk.current = new GateFiSDK({
-      merchantId: "testID",
+      merchantId: "0f2f9ecc-0da6-4760-bf2e-6b5a7fe797cd",
       displayMode: GateFiDisplayModeEnum.Overlay,
       nodeSelector: "#gatefi-button",
       // TODO: replace with AA wallet address
       walletAddress: "0xD71256eC24925873cE9E9F085f89864Ca05970bD",
+      isSandbox: true,
     });
 
     sdk.current.hide();
