9bb6e56915b43b4da56d4f318e562b4a1f15736b
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-11-19 02:52:28 +0300

    deploy script for "MoreComplexAccountFactory"

---
+++ apps/contracts/script/deploy-more-complex-account-factory.s.sol
@@ -0,0 +1,57 @@
+/* 
+forge script script/deploy-more-complex-account-factory.s.sol:DeployMoreComplexAccountFactory --rpc-url scroll-sepolia
+forge script script/deploy-more-complex-account-factory.s.sol:DeployMoreComplexAccountFactory --rpc-url scroll-sepolia --broadcast --verify
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+
+import {MoreComplexAccountFactory} from "src/MoreComplexAccountFactory.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+contract DeployMoreComplexAccountFactory is Script {
+    function run() external {
+        // Account to deploy from
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        address ENTRY_POINT = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy Implementation Contracts
+        new MoreComplexAccountFactory(IEntryPoint(ENTRY_POINT));
+
+        vm.stopBroadcast();
+    }
+}
+
+// Verify on Linea Goerli
+
+/*
+forge verify-contract \
+    --chain-id 59140 \
+    --num-of-optimizations 200 \
+    --watch \
+    --etherscan-api-key A2S2FFQ3VQHJWSGBU5UTT9UCIBJ2MZYA4P \
+    --compiler-version v0.8.20+commit.a1b79de6 \
+    --constructor-args "0x0000000000000000000000005ff137d4b0fdcd49dca30c7cf57e578a026d2789" \
+    0x3e6a716eae6c65171a79c6f532f6b4706aaa2e03 \
+    src/MoreComplexAccountFactory.sol:MoreComplexAccountFactory
+*/
+
+// Verify on Scroll Sepolia
+
+/*
+forge verify-contract \
+    --chain-id 534351 \
+    --num-of-optimizations 200 \
+    --watch \
+    --etherscan-api-key NQ5BXVFAMPVY5XXADFTKDIZBUST41UHSUG \
+    --compiler-version v0.8.20+commit.a1b79de6 \
+    --constructor-args "0x0000000000000000000000005ff137d4b0fdcd49dca30c7cf57e578a026d2789" \
+    0x3e6a716eae6c65171a79c6f532f6b4706aaa2e03 \
+    src/MoreComplexAccountFactory.sol:MoreComplexAccountFactory
+*/
