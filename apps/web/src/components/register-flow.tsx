0b96514328206cbb650072b91aa23af66444c342
ericzakariasson <eric.zakariasson@gmail.com>
2023-11-18 16:32:05 +0300

    feat: add gatefi sdk

--- apps/web/src/components/register-flow.tsx
+++ apps/web/src/components/register-flow.tsx
@@ -1,5 +1,26 @@
-'use client';
+"use client";
+
+import { GateFiSDK, GateFiDisplayModeEnum } from "@gatefi/js-sdk";
+import { useEffect, useRef } from "react";
+import { Button } from "../app/components/ui/button";
 
 export function RegisterFlow() {
-  return <div></div>;
+  const gatefiRef = useRef<GateFiSDK>();
+  useEffect(() => {
+    gatefiRef.current = new GateFiSDK({
+      merchantId: "",
+      displayMode: GateFiDisplayModeEnum.Overlay,
+      nodeSelector: "#gatefi-button",
+    });
+
+    return () => {
+      gatefiRef.current?.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <Button id="gatefi-button"></Button>
+    </div>
+  );
 }
