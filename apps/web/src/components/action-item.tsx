fb3e44fcd1722e8ea4cf7152cc3158a02e7cf912
ericzakariasson <eric.zakariasson@gmail.com>
2023-11-18 18:26:08 +0300

    chore: cleanup

--- apps/web/src/components/action-item.tsx
+++ apps/web/src/components/action-item.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react"
-import { Step, Trigger } from "./flow-builder"
-import { SelectActionDropDown } from "./select-action-dropdown"
-import { TokenSelect } from "./token-select"
-import clsx from "clsx"
-import { TrashCanIcon } from "./icons/trash-can-icon"
-import { set } from "zod"
-import Image from "next/image"
-import { ThinArrow } from "./icons/thin-arrow"
+import React, { useState } from "react";
+import { Step, Trigger } from "./flow-builder";
+import { SelectActionDropDown } from "./select-action-dropdown";
+import { TokenSelect } from "./token-select";
+import clsx from "clsx";
+import { TrashCanIcon } from "./icons/trash-can-icon";
+import { set } from "zod";
+import Image from "next/image";
+import { ThinArrow } from "./icons/thin-arrow";
 
 type ActionItemProps = {
-  trigger: Trigger | null
-  step: Step
-  onChange: (step: Step) => void
-  onRemoveStep: (step: Step) => void
-}
+  trigger: Trigger | null;
+  step: Step;
+  onChange: (step: Step) => void;
+  onRemoveStep: (step: Step) => void;
+};
 
 export const ActionItem = ({
   step,
@@ -21,14 +21,13 @@ export const ActionItem = ({
   onRemoveStep,
   trigger,
 }: ActionItemProps) => {
-  const [isHovered, setisHovered] = useState(false)
   const handleDropdownChange = (newValue: string) => {
-    onChange({ ...step, config: { ...step.config, type: newValue } })
-  }
+    onChange({ ...step, action: { ...step.action, type: newValue } });
+  };
 
   const handleRemoveStep = () => {
-    onRemoveStep(step)
-  }
+    onRemoveStep(step);
+  };
 
   const tokenOptions = [
     {
@@ -41,39 +40,39 @@ export const ActionItem = ({
       label: "swap 1inch",
       image: "/icons/1inch-logo.svg",
     },
-  ]
+  ];
 
-  let render: React.ReactNode = null
+  let render: React.ReactNode = null;
 
-  if (step.config.type === "SEND_PUSH_PROTOCOL_NOTIFICATION") {
-    console.log("hello")
+  if (step.action.type === "SEND_PUSH_PROTOCOL_NOTIFICATION") {
+    console.log("hello");
     render = (
       <div className="flex flex-col gap-4 p-4">
         <input
           className="w-full rounded-md bg-gray-100 px-4 py-2"
           placeholder="Title"
-          value={step.config.title}
+          value={step.action.title}
           onChange={(e) =>
             onChange({
               ...step,
-              config: { ...step.config, title: e.target.value },
+              action: { ...step.action, title: e.target.value },
             })
           }
         />
         <input
           className="w-full rounded-md bg-gray-100 px-4 py-2"
           placeholder="Message"
-          value={step.config.message}
+          value={step.action.message}
           onChange={(e) =>
             onChange({
               ...step,
-              config: { ...step.config, message: e.target.value },
+              action: { ...step.action, message: e.target.value },
             })
           }
         />
       </div>
-    )
-  } else if (step.config.type === "SWAP_ONE_INCH") {
+    );
+  } else if (step.action.type === "SWAP_ONE_INCH") {
     render = (
       <div className="flex gap-4 p-4 justify-start items-center">
         <div className="flex">
@@ -85,37 +84,25 @@ export const ActionItem = ({
 
         <div className="flex">
           <TokenSelect
-            value={step.config.type}
+            value={step.action.type}
             onChange={handleDropdownChange}
             options={tokenOptions}
           />
         </div>
       </div>
-    )
+    );
   }
 
   return (
-    <div
-      className="w-[600px] gap-4 bg-gray-200 rounded-md shadow-md flex flex-col relative"
-      onMouseEnter={() => setisHovered(true)}
-      onMouseLeave={() => setisHovered(false)}
-    >
-      {isHovered && (
-        <div
-          className="absolute top-0 right-0 translate-x-3 translate-y--6 rotate-[30deg] z-[999] cursor-pointer"
-          onClick={handleRemoveStep}
-        >
-          <TrashCanIcon />
-        </div>
-      )}
+    <div className="gap-4 bg-gray-200 rounded-md shadow-md flex flex-col relative w-full">
       <div className="flex w-full rounded-t-md bg-white h-1/3 py-4">
         <TokenSelect
-          value={step.config.type}
+          value={step.action.type}
           onChange={handleDropdownChange}
           options={tokenOptions}
         />
       </div>
       <div className="h-2/3  bg-gray-200">{render}</div>
     </div>
-  )
-}
+  );
+};
