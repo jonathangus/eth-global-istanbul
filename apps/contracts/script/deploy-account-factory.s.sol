2952b55e5345dc676ec5adea129aa96a6bbd4085
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-11-18 18:47:56 +0300

    updated deploy script

--- apps/contracts/script/deploy-account-factory.s.sol
+++ apps/contracts/script/deploy-account-factory.s.sol
@@ -1,6 +1,6 @@
 /* 
-forge script script/deploy-account-factory.s.sol:DeployComplexAccountFactory --rpc-url linea-testnet
-forge script script/deploy-account-factory.s.sol:DeployComplexAccountFactory --rpc-url linea-testnet --broadcast --verify
+forge script script/deploy-account-factory.s.sol:DeployComplexAccountFactory --rpc-url scroll-sepolia
+forge script script/deploy-account-factory.s.sol:DeployComplexAccountFactory --rpc-url scroll-sepolia --broadcast --verify
 */
 
 // SPDX-License-Identifier: MIT
@@ -16,14 +16,42 @@ contract DeployComplexAccountFactory is Script {
         // Account to deploy from
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-        address LINEA_ENTRY_POINT = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+        address ENTRY_POINT = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
 
         // Start broadcasting transactions
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy Implementation Contracts
-        new ComplexAccountFactory(IEntryPoint(LINEA_ENTRY_POINT));
+        new ComplexAccountFactory(IEntryPoint(ENTRY_POINT));
 
         vm.stopBroadcast();
     }
 }
+
+// Verify on Linea Goerli
+
+/*
+forge verify-contract \
+    --chain-id 59140 \
+    --num-of-optimizations 200 \
+    --watch \
+    --etherscan-api-key A2S2FFQ3VQHJWSGBU5UTT9UCIBJ2MZYA4P \
+    --compiler-version v0.8.20+commit.a1b79de6 \
+    --constructor-args "0x0000000000000000000000005ff137d4b0fdcd49dca30c7cf57e578a026d2789" \
+    0x3e6a716eae6c65171a79c6f532f6b4706aaa2e03 \
+    src/ComplexAccountFactory.sol:ComplexAccountFactory
+*/
+
+// Verify on Scroll Sepolia
+
+/*
+forge verify-contract \
+    --chain-id 534351 \
+    --num-of-optimizations 200 \
+    --watch \
+    --etherscan-api-key NQ5BXVFAMPVY5XXADFTKDIZBUST41UHSUG \
+    --compiler-version v0.8.20+commit.a1b79de6 \
+    --constructor-args "0x0000000000000000000000005ff137d4b0fdcd49dca30c7cf57e578a026d2789" \
+    0x3e6a716eae6c65171a79c6f532f6b4706aaa2e03 \
+    src/ComplexAccountFactory.sol:ComplexAccountFactory
+*/
